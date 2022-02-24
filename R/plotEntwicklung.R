4071757136ca531550896e6305c422ead63ca1d2
Ulf <ulf.schepsmeier@tum.de>
2022-02-24 12:50:49 +0100

    Labels variable gestallten

--- R/plotEntwicklung.R
+++ R/plotEntwicklung.R
@@ -3,6 +3,9 @@
 #' @author Ulf Schepsmeier
 #'
 #' @param data data.frame mit Daten der Sportart, die dargestellt werden soll
+#' @param date_breaks Einteilung der x-Achse; default: date_breaks = "3 months"
+#' @param date_minor_breaks Untereinteilung der x-Achse; default: minor_breaks = "1 month"
+#' @param labels Label der x-Achse; default: labels = date_format("%Y-%m")
 #'
 #' @return ggplot-Objekt
 #' @export
@@ -10,7 +13,7 @@
 #' @import plotly
 #'
 
-plotEntwicklung <- function(data) {
+plotEntwicklung <- function(data, date_breaks = "3 months", date_minor_breaks = "1 month", labels = date_format("%Y-%m")) {
   data <- data[!is.nan(data$Mean) & data$Mean > 1, ]
   data$ymin <- round(pmax(data$Min, data$Mean - 2 * data$Sd), 0)
   data$ymax <- round(pmin(data$Max, data$Mean + 2 * data$Sd), 0)
@@ -23,7 +26,7 @@ plotEntwicklung <- function(data) {
     geom_ribbon(aes(ymin = ymin, ymax = ymax), fill = "lightblue", alpha = 0.7) +
     facet_wrap(~Messwert, scales = "free_y") +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-    scale_x_date(date_breaks = "3 month", minor_breaks = "1 month", labels = date_format("%Y-%m"))
+    scale_x_date(date_breaks = date_breaks, date_minor_breaks = date_minor_breaks, labels = labels)
   #ggplotly(p)
   p
 }
