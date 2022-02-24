7dfe53d72fd0165300d21c11569399d6b1501010
Ulf <ulf.schepsmeier@tum.de>
2022-02-24 14:19:23 +0100

    CI und Trend als optionale Parameter für den Entwicklungsplot

--- R/plotEntwicklung.R
+++ R/plotEntwicklung.R
@@ -6,6 +6,8 @@
 #' @param date_breaks Einteilung der x-Achse; default: date_breaks = "3 months"
 #' @param date_minor_breaks Untereinteilung der x-Achse; default: minor_breaks = "1 month"
 #' @param labels Label der x-Achse; default: labels = date_format("%Y-%m")
+#' @param CI Konfidenzband (Confidence interval) anzeigen? default CI = TRUE
+#' @param trend Soll ein Trend ermittelt und angezeigt werden? default: trend = FALSE
 #'
 #' @return ggplot-Objekt
 #' @export
@@ -13,7 +15,8 @@
 #' @import plotly
 #'
 
-plotEntwicklung <- function(data, date_breaks = "3 months", date_minor_breaks = "1 month", labels = date_format("%Y-%m")) {
+plotEntwicklung <- function(data, date_breaks = "3 months", date_minor_breaks = "1 month", labels = date_format("%Y-%m"),
+                            CI = TRUE, trend = FALSE) {
   data <- data[!is.nan(data$Mean) & data$Mean > 1, ]
   data$ymin <- round(pmax(data$Min, data$Mean - 2 * data$Sd), 0)
   data$ymax <- round(pmin(data$Max, data$Mean + 2 * data$Sd), 0)
@@ -23,10 +26,17 @@ plotEntwicklung <- function(data, date_breaks = "3 months", date_minor_breaks = 
     geom_line() +
     ylab("") +
     xlab("") +
-    geom_ribbon(aes(ymin = ymin, ymax = ymax), fill = "lightblue", alpha = 0.7) +
     facet_wrap(~Messwert, scales = "free_y") +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
     scale_x_date(date_breaks = date_breaks, date_minor_breaks = date_minor_breaks, labels = labels)
-  #ggplotly(p)
+
+  if (CI) {
+    p <- p +
+      geom_ribbon(aes(ymin = ymin, ymax = ymax), fill = "lightblue", alpha = 0.7)
+  }
+  if (trend) {
+    p <- p +
+      geom_smooth(color = "red", se = FALSE)
+  }
   p
 }
